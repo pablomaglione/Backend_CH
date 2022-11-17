c44ba385c97b99bd0003cb1355369477b4e7363a
pablomaglione <pmaglione83@gmail.com>
2022-11-17 15:39:07 -0300

    Desafio 1

---
+++ productManager.js
@@ -0,0 +1,64 @@
+class ProductManager {
+
+    constructor () {
+        this.products = []
+    }
+
+    addProduct = (title, description, price, thumbail, code, stock) => {
+        const product = {
+            id: this.getProductID(),
+            title,
+            description,
+            price,
+            thumbail,
+            code,
+            stock,
+        }
+        const codeDuplicated = (prod) => (prod.code == product.code); //Busca si el code ya existe, si no existe carga el producto
+
+        if (!this.products.some(codeDuplicated)){
+            this.products.push(product)
+        } else {
+            console.log("Codigo duplicado")
+        }
+    }
+
+    getProducts = () => {
+        if(this.products.length > 0)
+            return this.products
+        else
+            return 0
+    }
+
+    getProductID = () => {
+        return (this.products.length + 1)
+    }
+
+    getProductByID = (productid) => {
+        const prod = this.products.find(p => p.id == productid)
+        
+        if(prod){
+            console.log(prod)}
+        else{
+            console.log("Not Found")}
+    }
+}
+
+
+//TEST
+const product = new ProductManager()
+
+console.log("Products: ", product.getProducts()) // Imprime todos los productos cargados o 0 sin no hay ninguno
+
+
+product.addProduct("titulo", "descrip", 100, "foto", 5, 10)
+product.addProduct("titulo2", "descrip2", 200, "foto", 6, 11)
+product.addProduct("titulo3", "descrip2", 200, "foto", 1, 11)
+
+console.log("Products: ", product.getProducts()) //Imprime todos los productos cargados
+
+product.getProductByID(5) // ID no exite
+product.getProductByID(2) // Imprime el producto con ID 2
+
+
+
